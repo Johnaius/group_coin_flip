dfa5df62022d20afc81ba80c5e6b78baed90b306
unknown <johnzak80@gmail.com>
2022-05-29 15:36:12 -0400

    update!

--- js/main.js
+++ js/main.js
@@ -1,30 +1,10 @@
-document.getElementById('headsButton').addEventListener('click', () => {
-  let choice = Math.floor(Math.random()* 2) 
-  if(choice === 1){
-    choice = 'Heads'
-    document.querySelector("p").innerHtml = "You Win"
-  } else if(choice !== 1){
-    document.querySelector('p').innerHTML = "You Lose!"
-  }
-  console.log(choice)
-})
+document.querySelector('.button').addEventListener('click', makeReq)
 
-document.getElementById('tailsButton').addEventListener('click', () => {
-  let choice = Math.floor(Math.random()* 2)
-  if(choice === 1){
-    choice = 'Tails'
-    document.querySelector("p").innerHtml = "You Win!"
-  } else if(choice !== 1){
-    document.querySelector('p').innerHTML = "You Lose!"
-  }
-  console.log(choice)
-})
+async function makeReq(){
 
-
-
-function alexIdeas() {
-  // Since our buttons are spans, we can select them all like this.
   const buttons = document.querySelectorAll('span')
+  const res = await fetch(`/api`)
+  const data = await res.json()
   
   // Then assign identical click events all at once.
   for (const button of buttons) {
@@ -33,5 +13,20 @@ function alexIdeas() {
         const result = Math.ceil(Math.random() * 2) === 1 ? 'You Win' : 'You Lose'
         document.querySelector("p").innerHTML = result
     })
-  }
 }
+}
+
+
+// function alexIdeas() {
+//   // Since our buttons are spans, we can select them all like this.
+//   const buttons = document.querySelectorAll('span')
+  
+//   // Then assign identical click events all at once.
+//   for (const button of buttons) {
+//     button.addEventListener('click', (e) => {
+//       // So each time we click a button, we'll run this logic no matter the button.
+//         const result = Math.ceil(Math.random() * 2) === 1 ? 'You Win' : 'You Lose'
+//         document.querySelector("p").innerHTML = result
+//     })
+//   }
+// }
--- server.js
+++ server.js
@@ -1,12 +1,49 @@
 const http = require('http');
-const fs = require('fs');
-http.createServer(function (req, res) {
+const fs = require('fs')
+const url = require('url');
+const querystring = require('querystring');
+const figlet = require('figlet')
+const server= http.createServer((req, res) => {
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  console.log(page);
+  if (page == '/') {
   fs.readFile('index.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
   res.end();
   });
-}).listen(8000);
+}else if (page == '/api') {
+   res.writeHead(200, {'Content-Type': 'application/json'});
+      let flipRes = Math.ceil(Math.random()*2)===1 ? "heads" : "tails" 
+      const objToJson = {
+        flip: flipRes
+      }
+      res.end(JSON.stringify(objToJson));
+    }else if (page == '/css/style.css'){
+      fs.readFile('css/style.css', function(err, data) {
+        res.write(data);
+        res.end();
+      });
+    }else if (page == '/js/main.js'){
+      fs.readFile('js/main.js', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/javascript'});
+        res.write(data);
+        res.end();
+      });
+  }else{
+    figlet('404!!', function(err, data) {
+      if (err) {
+          console.log('Something went wrong...');
+          console.dir(err);
+          return;
+      }
+      res.write(data);
+      res.end();
+})
+  }
+});
+server.listen(8000);
 
 // I think the query selectors will go in the frontend (script.js), while we could do the coinflip on server.js.
 // Then we would need to fetch from script.js to get the coinflip result.
